f7e5886b7ac6b2b37797be3888580778c86808a9
traveler0027 <54610074+traveler0027@users.noreply.github.com>
2020-09-09 13:31:56 -0500

    reuploaded package files

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,26 @@
-const path = require('path');
+// const path = require('path');
+
+// module.exports = {
+//   entry: './src/index.js',
+//   output={
+//     path: path.resolve('public'),
+//     filename: 'bundle.js',
+//   },
+
+//   module: {
+//     rules: [
+//       {
+//         test: /\.js$/,
+//         exclude: /node_modules/,
+//         use: {
+//           loader: 'babel-loader',
+//         },
+//       },
+//     ],
+//   },
+// };
 
 module.exports = {
-  entry: './src/index.js',
-  output={
-    path: path.resolve('public'),
-    filename: 'bundle.js',
-  },
-
   module: {
     rules: [
       {
